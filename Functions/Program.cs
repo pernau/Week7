b5810bc117002ab2ae38f1e6079fa9737409eada
Heleri Pernau <hpernau@tktk.ee>
2021-10-22 14:28:08 +0300

    control flow

---
+++ Functions/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta esimest arvu:");
+            int UserNumberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sisesta teist arvu:");
+            int userNumberTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Palun vali tehe (sisesta '+', '-', '*' või '/')");
+            char userOperator = Convert.ToChar(Console.ReadLine());
+
+            switch (userOperator)
+            {
+                case '+':
+                    AddTwoNumbers(UserNumberOne, userNumberTwo);
+                    break;
+                case '/':
+                    DivideTwoNumbers(UserNumberOne, userNumberTwo);
+                    break;
+                case '-':
+                    TakeAwayTwoNumbers(UserNumberOne, userNumberTwo);
+                    break;
+                case '*':
+                    MultipyTwoNumbers(UserNumberOne, userNumberTwo);
+                    break;
+                default:
+                    Console.WriteLine("kena päeva!");
+                    break;
+            }
+        }
+
+        private static void AddTwoNumbers(int x, int y)
+        {
+            int result = x + y;
+            Console.WriteLine(result);
+        }
+
+        public static void DivideTwoNumbers(double x, double y)
+        {
+            double result = x / y;
+            Console.WriteLine(result);
+        }
+
+        private static void MultipyTwoNumbers(int x, int y)
+        {
+            int result = x * y;
+            Console.WriteLine(result);
+        }
+
+        private static void TakeAwayTwoNumbers(int x, int y)
+        {
+            int result = x - y;
+            Console.WriteLine(result);
+        }
+    }
+
+}
---
+++ UserNameDataTask/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace UserNameDataTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada eesnime
+            //programm küsib kasutajal sisestada numbri 1-3
+            //kui kasutaja sisestab 1, siis kuvatakse eesnime tagurpidi
+            //kui kasutaja sisestab 2, siis kuvatakse eesnime esimest tähte
+            //kui kasutaja alib 3, siis kuvatakse kasutaja eesnime pikkust
+
+            Console.WriteLine("Sisesta enda eesnimi:");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Vali number 1, 2 või 3");
+            char userNumber = Convert.ToChar(Console.ReadLine());
+
+            switch (userNumber)
+            {
+                case '1':
+                    GetUserNameData1(userName);
+                    break;
+                case '2':
+                    GetUserNameData2(userName);
+                    break;
+                case '3':
+                    GetUserNameData3(userName);
+                    break;
+
+            }
+        }
+
+        public static void GetUserNameData1 (string userInput)
+        {
+            for (int i = userInput.Length - 1; i >= 0; i--) 
+            {
+                Console.Write(userInput[i]);
+            }
+        }
+
+        public static void GetUserNameData2 (string userInput)
+        {
+            Console.WriteLine($"Sinu eesnime esimene täht on {userInput[0]}");
+        }
+
+        public static void GetUserNameData3 (string userInput)
+        {
+            Console.WriteLine($"Sinu nimi on {userInput.Length} sümbolit pikk");
+        }
+
+    }
+}
